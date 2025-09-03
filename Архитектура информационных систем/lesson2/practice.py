f873ce125597ccaeb9650f25286141158917909c
Skugb <maximkom1@mail.ru>
2025-09-03 14:39:41 +0500

    New bsababa

--- Архитектура информационных систем/lesson2/practice.py
+++ Архитектура информационных систем/lesson2/practice.py
@@ -3,3 +3,13 @@
 выветси на консоль больше ли ваш возраст чем 18
 определить тип переменной возраста, вывести на консоль
 """
+
+years = 21
+number = years
+print(number)
+
+flag = True
+flag2 = False
+result = number > 18
+print(result)
+print(type(result))
