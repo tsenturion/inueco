59935fdb22e369bfd413ea74fae71cd475a76e6d
Tema13cm <azeevartem0@gmail.com>
2025-09-03 16:18:54 +0500

    Main 234

--- Архитектура информационных систем/lesson2/2pz.py
+++ Архитектура информационных систем/lesson2/2pz.py
@@ -2,3 +2,20 @@
 # 2. Запросите операцию (+, -, *, /)
 # 3. Используя if/elif/else, выполните нужную операцию
 # 4. Выведите результат и его тип
+x = int(input("введите первое число: "))
+y = int(input("введите второе число: "))
+math_operation = input('введите действие(+ - * /): ')
+if math_operation == "+":
+    result = x + y
+    print(result, type(result))
+elif math_operation == "-":
+    result = x - y
+    print(result, type(result))
+elif math_operation == "*":
+    result = x * y
+    print(result, type(result))
+elif math_operation == "/":
+    result = x / y
+    print(result, type(result))
+else:
+    print("ошибка: операция не корректна")
