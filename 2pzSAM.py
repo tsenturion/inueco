390126cf3acb99766cf64bd7f741fec46a5223d5
Skugb <maximkom1@mail.ru>
2025-09-03 15:54:08 +0500

    2 Практическая работа

---
+++ 2pzSAM.py
@@ -0,0 +1,30 @@
+# 1. Запросите два числа
+# 2. Запросите операцию (+, -, *, /)
+# 3. Используя if/elif/else, выполните нужную операцию
+# 4. Выведите результат и его тип
+
+nu1 = int(input( ))
+nu2 = int(input( ))
+
+oper = str(input("Операция + - * /" ))
+
+if oper == "+":
+    answ = nu1 + nu2
+    print(answ)
+    print(type(answ))
+elif oper == "-":
+    answ = nu1 - nu2
+    print(answ)
+    print(type(answ))
+elif oper == "*":
+    answ = nu1 * nu2
+    print(answ)
+    print(type(answ))
+elif oper == "/":
+    answ = nu1 / nu2
+    print(answ)
+    print(type(answ))
+else: 
+    print("ERROR: WRONG SYNT.")
+    
+    
