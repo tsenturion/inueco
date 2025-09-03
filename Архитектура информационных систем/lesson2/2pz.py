3663e6c6a8a2cb3c594b3cf8f3174c9ad1155811
1VanLin1 <125788702+1VanLin1@users.noreply.github.com>
2025-09-03 16:32:57 +0500

    2pz_test

--- Архитектура информационных систем/lesson2/2pz.py
+++ Архитектура информационных систем/lesson2/2pz.py
@@ -2,3 +2,30 @@
 # 2. Запросите операцию (+, -, *, /)
 # 3. Используя if/elif/else, выполните нужную операцию
 # 4. Выведите результат и его тип
+
+a = float(input("Введите первое число:"))
+b = float(input("Введите второе число:"))
+
+operation = input("Введите операцию (+, -, *, /): ")
+
+result = None
+
+if operation == "+":
+    result = a + b
+elif operation == "-":
+    result = a - b
+elif operation == "*":
+    result = a * b
+elif operation == "/":
+    if b != 0:
+        result = a / b
+    else:
+        print("Ошибка: деление на ноль!")
+        result = None
+else:
+    print("Ошибка: неизвестная операция!")
+    result = None
+
+if result is not None:
+    print(f"Резудьтат: {result}")
+    print(f"Тип результата: {type(result)}")
