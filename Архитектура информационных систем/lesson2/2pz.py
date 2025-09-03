6fbb999a1524c1fa90865ec01928fc79df4d4846
novans111 <ipavlov9767@gmail.com>
2025-09-03 16:05:27 +0500

    2

--- Архитектура информационных систем/lesson2/2pz.py
+++ Архитектура информационных систем/lesson2/2pz.py
@@ -2,9 +2,13 @@
 # 2. Запросите операцию (+, -, *, /)
 # 3. Используя if/elif/else, выполните нужную операцию
 # 4. Выведите результат и его тип
+
+
+
+
 number1 = int(input("введите 1 число: "))
 number2 = int(input("введите 2 число: "))
-x = (input)("operation")
+x = input("operation")
 if x == "+":
     print(number1+number2)
 if x == "-":
@@ -13,3 +17,4 @@ if x == "*":
     print(number1*number2)
 if x == "/":
     print(number1/number2)
+print(type(x))
