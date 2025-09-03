96796b826fe2fd0e5f17c4b2df5db8642ff4ae85
Hellsi27 <artemeliseev280@gmail.com>
2025-09-03 16:30:04 +0500

    Eliseev

--- Архитектура информационных систем/lesson2/2pz.py
+++ Архитектура информационных систем/lesson2/2pz.py
@@ -2,3 +2,34 @@
 # 2. Запросите операцию (+, -, *, /)
 # 3. Используя if/elif/else, выполните нужную операцию
 # 4. Выведите результат и его тип
+number = int 
+print(number)
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+message = ''' Выберете математическую операцию:
+
++ : Сложение
+- : Вычитание
+/ : Деление
+* : Умножение
+
+Ваш выбор:
+'''
+operation = input(message)
+if operation == '+':
+    print('Сложение')
+    result = num1 + num2
+elif operation == '-':
+    print('Вычитание')
+    result = num1 - num2
+elif operation == '/':
+    print('Деление')
+    result = num1 / num2
+elif operation == '*':
+    print('Умножение')
+    result = num1 * num2
+else:
+    print('Неизвестная операция')
+
+
+print("Результат:", result)
