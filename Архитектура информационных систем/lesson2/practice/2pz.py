ea3374602da150a0b01356745cc0bc3e39a17789
Egoryich <24301055@live.inueco.ru>
2025-09-10 16:07:28 +0500

    Практическая 2 03.09.2025

---
+++ Архитектура информационных систем/lesson2/practice/2pz.py
@@ -0,0 +1,22 @@
+# 1. Запросите два числа
+# 2. Запросите операцию (+, -, *, /)
+# 3. Используя if/elif/else, выполните нужную операцию
+# 4. Выведите результат и его тип
+
+print("Welcome to calculator")
+number_1 = int(input("Введите число:     ")) 
+oper = input("ВВедите оператор:     ")
+number_2 = int(input("Введите число:    ")) 
+
+if oper == '-':
+    res = number_1 - number_2
+elif oper == '+':
+    res = number_1 + number_2
+elif oper == '*':
+    res = number_1 * number_2
+elif oper == '/':
+    res = number_1 / number_2
+else: 
+    print("Неизвестный оператор")
+print("Результат: " + str(res))
+print(type(res))
