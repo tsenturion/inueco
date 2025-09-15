dd51dc3ac9a2f43c6ff106e0c09c36d05935434f
MrSlime228 <167946033+MrSlime228@users.noreply.github.com>
2025-09-15 19:36:21 +0500

    Практика 3 (Пляшник  А)

--- Управление качеством и надежность информационных систем/lesson 3,4/pz3.py
+++ Управление качеством и надежность информационных систем/lesson 3,4/pz3.py
@@ -2,3 +2,15 @@
 вводить возраст с консоли и печатать его на экран. Возраст должен быть int
 пока не будет введено exit
 """
+
+
+flag = False
+while flag != True:
+    age = input("Введите свой возраст: ")
+    if age == "exit" or age == "Exit" or age == "EXIT":
+        flag = True
+    else: int(age)
+    print (age)
+else:
+    print("Введён Exit завершаем цикл!")
+
