88d858261c10aa17a0294b092af109a173daa14e
roovethelema <roovethelema@gmail.com>
2025-09-15 15:17:55 +0500

    PZ6

---
+++ Архитектура информационных систем/lesson3/pz6.py
@@ -0,0 +1,32 @@
+students = {
+    101: {
+        "name": "Иван Иванов",
+        "age": 20,
+        "grades": {"math": 4, "physics": 5, "programming": 5},
+        "group": "ИТ-101"
+    },
+    102: {
+        "name": "Мария Петрова", 
+        "age": 19,
+        "grades": {"math": 5, "physics": 4, "programming": 4},
+        "group": "ИТ-101"
+    },
+    103: {
+        "name": "Алексей Сидоров",
+        "age": 21, 
+        "grades": {"math": 3, "physics": 3, "programming": 4},
+        "group": "ИТ-102"
+    }
+}
+
+best_student = None
+best_avg = 0
+
+for student in students.values():
+    grades = student["grades"].values()
+    avg = sum(grades) / len(grades)
+    if avg > best_avg:
+        best_avg = avg
+        best_student = student["name"]
+
+print(best_student, best_avg)
