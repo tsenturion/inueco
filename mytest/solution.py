a4338e424072f82bc58b5e2a90941a19038c08ed
XAMAH <131152080+XAMAH1@users.noreply.github.com>
2025-10-06 12:23:16 +0500

    Качалкин Илья ИЗу-201

--- mytest/solution.py
+++ mytest/solution.py
@@ -1,3 +1,17 @@
-def analyze_temperature(temperatures):
-
-if __name__ == "__main__":
+def analyze_temperature(temperatures: list):
+    if len(temperatures) < 1 or len(temperatures) != 7:
+        return None
+    hot_days = 0
+    cold_days = 0
+    for current_day in temperatures:
+        if current_day > 25:
+            hot_days += 1
+        elif current_day < 10:
+            cold_days += 1
+    return {
+        "average": round(sum(temperatures) / len(temperatures), 1), 
+        "max": max(temperatures), 
+        "min": min(temperatures), 
+        "hot_days": hot_days, 
+        "cold_days": cold_days
+        }
--- mytest/solution2.py
+++ mytest/solution2.py
@@ -1,3 +1,16 @@
-def analyze_purchases(items, prices, discount_threshold=1000):
-
-if __name__ == "__main__":
+def analyze_purchases(items: list[str], prices: list[int], discount_threshold=1000):
+    if len(items) < 1: 
+        return None
+    if len(items) != len(prices) or discount_threshold <= 0 or min(prices) < 0:
+        return None
+    applied = 0
+    if discount_threshold <= sum(prices):
+        applied = 10 
+    return {
+        "total": sum(prices),
+        "average": round(sum(prices) / len(prices), 2),
+        "most_expensive": items[prices.index(max(prices))],
+        "discount_applied": bool(applied),
+        "final_total": round(sum(prices) * (1 - applied * 0.01), 2)
+    }
+    
