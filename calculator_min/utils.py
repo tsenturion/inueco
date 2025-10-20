2daa3872b470e32f8ad30cdb600e1749cae9fb91
MozgiNaMeste <maximkom1@mail.ru>
2025-10-20 15:16:09 +0500

    20.10.2025 task done
    Eryukov M.M.

--- calculator_min/utils.py
+++ calculator_min/utils.py
@@ -1,6 +1,7 @@
-#поправить по pep8 (flake8), по pylance, по sonarqube, по pylint
-#python -m pylint calculator_min\utils.py
-#python -m flake8 calculator_min\utils.py
+"""
+Нужен был doctring, я сделяль
+"""
+
 import math
 
 
@@ -9,25 +10,31 @@ def degrees_to_radians(degrees: int) -> float:
     return degrees * (math.pi / 180.0)
 
 
-def radians_to_degrees(radians):
+def radians_to_degrees(radians: int) -> float:
     """Преобразование радианов в градусы"""
     return radians * (180.0 / math.pi)
 
-def format_operation(operation_name, operands, result):
+
+def format_operation(operation_name: str, operands: list[float], result: str):
     """Форматирование операции для красивого вывода"""
     if len(operands) == 1:
         return f"{operation_name}({operands[0]}) = {result}"
     else:
         return f"{operands[0]} {operation_name} {operands[1]} = {result}"
 
-def format_result(operation, result):
+
+def format_result(result: str):
     """Форматирование результата вычисления"""
     return f"Результат: {result}"
 
-def is_integer(number):
-    """Проверка, является ли число целым"""
-    return isinstance(number, int) or (isinstance(number, float) and number.is_integer())
 
-def is_positive(number):
+def is_integer(number: float | int) -> bool:
+    """Проверка, является ли число целым"""
+    if isinstance(number, int):
+        return True
+    return number.is_integer()
+
+
+def is_positive(number: float) -> float:
     """Проверка, является ли число положительным"""
     return number > 0
