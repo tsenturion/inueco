5409b245f5f53d9208a4d6f396f1ed5d158c2948
ttyyryyhgfdd <128968538+ttyyryyhgfdd@users.noreply.github.com>
2026-01-23 11:31:02 +0500

    Шакиров И-107

---
+++ Архитектура информационных систем/i-107/lesson2/zadanie1.py
@@ -0,0 +1,35 @@
+#Шакиров Артём Ринатович И-107
+
+#1
+print('Hello, World!')
+
+#2
+# You know nothing, Jon Snow!
+
+#3
+print('Robert')
+print('Stannis')
+print('Renly')
+
+#4
+print(10)
+
+#5
+print("What Is Dead May Never Die")
+
+#6
+print(81/9)
+
+#7
+n=6
+m=-81
+print(n-m)
+
+#8
+print(3**5)
+print(-8/-4)
+
+#9
+m=8/2
+n=-3/2
+print(m+5-n)
