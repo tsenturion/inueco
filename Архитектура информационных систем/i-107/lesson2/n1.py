3b04f953058c05f280be95e551d44f1e109470ea
noyMMeS <honoradla640@gmail.com>
2026-01-23 11:32:00 +0500

    Марков И - 107

---
+++ Архитектура информационных систем/i-107/lesson2/n1.py
@@ -0,0 +1,25 @@
+
+# 1
+print('Hello, World!')
+
+# 2
+# You know nothing, Jon Snow!
+
+# 3
+print('Robert')
+print('Stannis')
+print('Renly')
+
+# 4
+print(10)
+
+# 5
+print('What Is Dead May Never Die')
+
+# 6
+
+a = 81
+b = 9
+print(a/b)
+
+
