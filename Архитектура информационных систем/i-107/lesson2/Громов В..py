d2b9d4e0badd28cf8f1e78de026a5ea6bb71bb2c
L2ufu <vladgromov574@gmail.com>
2026-01-23 11:26:03 +0500

    Громов В. И-107

---
+++ Архитектура информационных систем/i-107/lesson2/Громов В..py
@@ -0,0 +1,19 @@
+#1 привет мир
+print('Hello, World!')
+
+#2 комментарии
+# For Winterfell!
+# For Lanisters!
+
+#3 Инструкции (Statements)
+print('Mother of Dragons.') # Первая инструкция
+print('Dracarys!') # Вторая инструкция
+
+#4 Как мы проверяем ваши решения
+print(10)
+
+#5 Синтаксические ошибки
+print('Hodor')
+      
+# 6 Арифметические операции
+print(81/9)
