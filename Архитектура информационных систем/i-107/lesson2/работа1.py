c8bcba400cccfa17f2ae61ae9d0b7f6be601e58c
3APA3OHbKA <safina_lerochka@bk.ru>
2026-01-30 11:50:32 +0500

    Сафига и-107

---
+++ Архитектура информационных систем/i-107/lesson2/работа1.py
@@ -0,0 +1,10 @@
+print('Hello, World!')
+# You know nothing, Jon Snow! 
+print("Robert")
+print("Stannis")
+print("Renly")
+print(10)
+print("What Is Dead May Never Die")
+print(81/9)
+print(6-81)
+
