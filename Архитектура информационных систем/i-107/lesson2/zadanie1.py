1df76d15256f908a69e7e11987f1d8287c3ab186
412zTEDDYz <belka9870@gmail.com>
2026-01-30 10:40:11 +0000

    Беляев Артём и-107

---
+++ Архитектура информационных систем/i-107/lesson2/zadanie1.py
@@ -0,0 +1,21 @@
+#Беляев Артём Александрович И-107
+#test
+#1
+print('Hello, World!')
+
+#2
+# You know nothing, Jon Snow!
+
+#3
+print('Robert')
+print('Stannis')
+print('Renly')
+
+#4
+print(10)
+
+#5
+print("What Is Dead May Never Die")
+
+#6
+print(81/9)
