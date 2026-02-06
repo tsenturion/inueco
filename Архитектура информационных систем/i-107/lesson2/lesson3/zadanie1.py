38d3f1f8bd33b0633f4209a550e13013fa51c48f
Серебряков Артем <serebro238333@gmail.com>
2026-02-06 10:52:09 +0500

    Серебряков и-107

---
+++ Архитектура информационных систем/i-107/lesson2/lesson3/zadanie1.py
@@ -0,0 +1,16 @@
+#Серебряков Артем Александрович И-107
+#test
+# 1 Привет, Мир
+print('Hello, World!')
+# 2 Комментарии
+# You know nothing, Jon Snow!
+# 3 Инструкции (Statements)
+print('Robert')
+print('Stannis')
+print('Renly')
+# 4 Как мы проверяем ваши решения
+print(10)
+# 5 Синтаксические ошибки
+print('What Is Dead May Never Die')
+# 6 Арифметические операции
+print(81 / 9)
