e7d2c651743ef2954f20a64e4ea77fec4ad0e5bb
2rmq4g9kp2-alt <2rmq4g9kp2@privaterelay.appleid.com>
2026-02-06 11:39:14 +0500

    бузуев и-107

---
+++ Архитектура информационных систем/i-107/lesson2/fege.py
@@ -0,0 +1,22 @@
+# 1 
+print(70 * (3 + 4) / (8 + 2))
+# 2
+print (0.39 * 0.22)
+# 3
+print((5**2)-(3*7))
+# 4
+print('"Khal Drogo\'s favorite word is "athjahakar""')
+# 5
+print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+# 6
+print("Winter " + "came " + "for " + "the " + "House " + "of " + "Frey.")
+# 7
+print(chr(126))
+print(chr(94))
+print(chr(37))
+# 8
+motto = "What Is Dead May Never Die!"
+print (motto)
+
+
+
