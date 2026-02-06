57bd4d72eb18b58865e68fe436827641c14691f3
TheAntongo <antoxagg09@gmail.com>
2026-02-06 11:19:41 +0500

    Ширкин Антон И-107

---
+++ Архитектура информационных систем/i-107/lesson4/dfdfsdfsf.py
@@ -0,0 +1,17 @@
+#Операторы
+print(6-(-81))
+#Коммутативная операция
+print(3**5)
+print((-8)/(-4))
+#Композиция операций
+print(8/2+5-(-3)/2)
+#Приоритет
+print(70*(3+4)/(8+2))
+#Числа с плавающей точкой
+print(0.39*0.22)
+#Линтер
+print( (5 **2)-(3* 7))
+#Кавычки
+print('"Khal Drogo\'s favorite word is "athjahakar""')
+#Экранированные последовательности
+print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
