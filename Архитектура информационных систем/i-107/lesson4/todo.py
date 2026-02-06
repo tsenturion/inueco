f8f319f712be7d40b1fdc12d64b766a636df2128
mrvxss <mariarasikova905@gmail.com>
2026-02-06 11:38:09 +0500

    Update todo.py

--- Архитектура информационных систем/i-107/lesson4/todo.py
+++ Архитектура информационных систем/i-107/lesson4/todo.py
@@ -9,3 +9,32 @@ https://stepik.org/catalog/131?free=true
 "Поколение Python": курс для начинающих 
 до 2.3 Не включая
 """
+#1 привет мир
+print('Hello, World!')
+#2 комментарии
+# You know nothing, Jon Snow!
+#3 инструкции (Statements)
+print('Robert')
+print('Stannis')
+print('Renly')
+#4 как мы проверяем ваши решения
+print(10)
+#5 синтаксические ошибки
+print('What Is Dead May Never Die')
+#6 арифметические операции
+print(81/9)
+#7 Python: Операторы
+print(6 - - 81)
+#8 Python: Коммутативная операция
+print(3 ** 5)
+print(-8 / -4)
+#9 Python: Композиция операций
+print(8 / 2 + 5 - -3 / 2)
+#10 Python: Приоритет
+print(70 * (3 + 4) / (8 + 2))
+#11 Python: Числа с плавающей точкой
+print(0.39 * 0.22)
+#12 Python: Линтер
+print((5 ** 2) - (3 * 7))
+#13 Python: Кавычки
+print('"Khal Drogo\'s favorite word is "athjahakar""')
