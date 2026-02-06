4333b22a02c92040452be8e0b9d6e3680bbae1cc
totalEXE <totalexe7543@gmail.com>
2026-02-06 11:23:37 +0500

    Dreditov Roman

---
+++ Dreditov.py
@@ -0,0 +1,35 @@
+#3
+a = 3**5
+b = -8
+c = -4
+print(a)
+print(b/c)
+
+#4
+a = 8
+b = 2
+c = 5 
+d = -3
+print(a / b + c - d / b)
+
+#5
+
+a = 70
+b = 3
+c = 4
+d = 8
+e = 2
+print(a * (b + c) / (d + e))
+
+#6
+
+print(0.39 * 0.22)
+
+#7
+
+print((5 **2)-(3* 7))
+
+#8
+
+print('"Khal Drogo\'s favorite word is "athjahakar""')
+
