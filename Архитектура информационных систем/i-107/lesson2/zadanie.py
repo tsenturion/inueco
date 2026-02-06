8cb45a07f2fd6ac53087a92bfc5fa84982296e8d
Fle1ron <seregau444@gmail.com>
2026-02-06 11:25:17 +0500

    Юдин Евгений И-107

---
+++ Архитектура информационных систем/i-107/lesson2/zadanie.py
@@ -0,0 +1,21 @@
+#Юдин Евгений Сергеевич
+#test
+#1
+print('Hello, World!')
+
+#2
+# You Know nothing, Jon Snow!
+
+#3
+print('Robert')
+print('Stannis')
+print('Renly')
+
+#4
+print('10')
+
+#5
+print('What Is dead May Never Die')
+#test
+#6
+print('81/9')
