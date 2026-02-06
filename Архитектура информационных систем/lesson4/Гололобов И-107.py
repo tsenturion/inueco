e52d75ee3b579758d644f92946facbf7d2cbc57a
Maksimiliano 69 <gololobovmaksim498@gmail.com>
2026-02-06 09:18:19 +0300

    gololobov i-107

---
+++ Архитектура информационных систем/lesson4/Гололобов И-107.py
@@ -0,0 +1,40 @@
+#Python: Композиция операций
+print(8/2+5--3/2)
+#Python: Приоритет
+print(70*(3+4)/(8+2))
+#Python: Числа с плавающей точкой
+print (0.39*0.22)
+#Python: Линтер
+print((5**2)-(3*7))
+#Python: Кавычки
+print("\"Khal Drogo's favorite word is \"athjahakar\"\"")
+#Python: Экранированные последовательности
+print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+#Python: Конкатенация
+print('Winter ' + 'came ' + 'for ' + 'the ' + 'House ' + 'of ' + 'Frey.')
+#Python: Кодировка
+print(chr(126))  # ~
+print(chr(94))   # ^
+print(chr(37))   # %
+#Python: Что такое переменная
+motto = 'What Is Dead May Never Die!'
+print(motto)
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
