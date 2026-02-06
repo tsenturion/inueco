12ce9a4b309bcacc20dcb4a78613874aa1b09e36
Серебряков Артем <serebro238333@gmail.com>
2026-02-06 11:36:10 +0500

    Серебряков т-107

--- Архитектура информационных систем/lesson4/zadanie2.py
+++
@@ -1,9 +0,0 @@
-#Серебряков Артем И-107
-#print(6--81)
-#print(3**5)
-#print(-8/-4)
-#print(8/2+5--3/2)
-#print(70*(3+4)/(8+2))
-#print(0.39*0.22)
-#print((5**2)-(3*7))
-#print('"Khal Drogo\'s favorite word is "athjahakar""')
