8288446e808e8237e0e353175a097319c08ad794
iv33924 <iv33924@gmail.com>
2026-02-06 11:14:56 +0500

    basics.com

--- Архитектура информационных систем/i-107/lesson2/zadanie1.py
+++ Архитектура информационных систем/i-107/lesson2/zadanie1.py
@@ -12,3 +12,20 @@ print(10)
 print('What Is Dead May Never Die')
 # 6 Арифметические операции
 print(81 / 9)
+# 7 Операторы 
+print(6 - -81)
+# 8 Коммутативная операция
+print(3 ** 5)
+print(-8 / -4)
+# 9 Композиция операций
+print(8 / 2 + 5 - -3 / 2)
+# 10 Приоритет
+print(70 * (3 + 4) / (8 + 2))
+# 11 Числа с плавающей точкой
+print(0.39 * 0.22)
+# 12 Линтер
+print((5 ** 2) - (3 * 7))
+# 13 Кавычки
+print("\"Khal Drogo's favorite word is \"athjahakar\"\"")
+# 14 Экранированные последовательности
+print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
