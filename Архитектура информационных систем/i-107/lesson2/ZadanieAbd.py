45c33938923cfc95917e9ef7e6a6935975426bb4
Fredyla <24300455@live.inueco.ru>
2026-02-06 11:30:57 +0500

    1-20 zadania Abdull Tim

--- Архитектура информационных систем/i-107/lesson2/ZadanieAbd.py
+++ Архитектура информационных систем/i-107/lesson2/ZadanieAbd.py
@@ -12,3 +12,41 @@ print(10)
 print('What Is Dead May Never Die')
 # 6 Арифметические операции
 print(81 / 9)
+# 7 Операторы
+print(6 - -81)
+# 8 Коммутативная операция
+print(3 ** 5)
+print(-8 / -4) 
+# 9 Композиция операций
+print(8 / 2 + 5 - -3 / 2)
+# 10 Приоритет
+print(70 * (3 + 4) / (8 + 2))
+# 11 Числа с плавающей точкой
+print(0.39 * 0.22)
+# 12 Линтер
+print((5 ** 2)-(3 * 7))
+# 13 Кавычки
+print('"Khal Drogo\'s favorite word is "athjahakar""')
+# 14 Экранированные последовательности
+print('- Did Joffrey agree?\n- He did. He also said "I love using \\n".')
+# 15 Конкатенация
+print('Winter ' + 'came for the House of Frey.')
+# 16 Кодировка
+print(chr(126))
+print(chr(94))
+print(chr(37))
+# 17 Что такое переменная
+motto = 'What Is Dead May Never Die!'
+print(motto)
+# 18 Изменение переменной
+name = 'anneirB'  
+print(name) 
+# 19 Выбор имени переменной
+x = 'Father!'
+x = 2
+print(x)
+# 20 Выражения в определениях
+euros_count = 100 * 1.25
+dollars_count = euros_count * 6.91
+print(euros_count)
+print(dollars_count)
