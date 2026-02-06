8925460de903123551cf35ce22c75f8bd53690bc
papazahoditvofis <stalkertuo@gmail.com>
2026-02-06 11:20:38 +0500

    Update todo.py

--- Архитектура информационных систем/i-107/lesson4/todo.py
+++ Архитектура информационных систем/i-107/lesson4/todo.py
@@ -9,3 +9,15 @@ https://stepik.org/catalog/131?free=true
 "Поколение Python": курс для начинающих 
 до. Не включая
 """
+#print('Hello, World!')
+## You know nothing, Jon Snow!
+#print('Robert','Stannis','Renly',sep='\n')
+#print('What Is Dead May Never Die')
+#print(81/9)
+#print(6-(-81))
+#print(3**5,(-8)/(-4),sep='\n')
+#print(70*(3+4)/(8+2))
+#print(0.39*0.22)
+#print((5**2)-(3*7))
+#print('"Khal Drogo\'s favorite word is "athjahakar""')
+#print("- Did Joffrey agree?",'- He did. He also said "I love using \\n".',sep='\n')
