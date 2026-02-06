4527723e7b5db530734f50c194ce686c181782d1
noyMMeS <honoradla640@gmail.com>
2026-02-06 11:22:40 +0500

    Markov i - 107

---
+++ Архитектура информационных систем/i-107/lesson4/zadanya.py
@@ -0,0 +1,37 @@
+#7
+a = 6
+b = -81
+print(a-b)
+#8
+a = 3
+b = 5
+c = -8
+d = -4
+print(a**b)
+print(c/d)
+#9
+a = 8
+b = 2
+c = -3
+d = 5
+print(a/b+d-c/b)
+#10
+print(70 * (3 + 4) / (8 + 2))
+#11
+print(0.39*0.22)
+#12
+print((5**2)-(3*7))
+#13
+print('"Khal Drogo\'s favorite word is \"athjahakar\""')
+#17
+motto = "What Is Dead May Never Die!"
+print(motto)
+#18
+name = "Brienna"
+
+# BEGIN (write your solution here)
+name = "anneirB"
+# END
+
+print(name)
+
