7ed6d3f6dd5db41dd2b72572ffde78193485ae94
ttyyryyhgfdd <128968538+ttyyryyhgfdd@users.noreply.github.com>
2026-02-06 11:39:49 +0500

    Шакиров И-108

--- Архитектура информационных систем/i-107/lesson4/zadanie4.py
+++ Архитектура информационных систем/i-107/lesson4/zadanie4.py
@@ -19,3 +19,63 @@ print("What Is Dead May Never Die")
 
 #6
 print(81/9)
+
+#7
+print(6-(-81))
+
+#8
+print(3**5)
+print(-8/-4)
+
+#9
+print(8/2+5-(-3/2))
+
+#10
+print(70*(3 + 4)/(8+2))
+
+#11
+print(0.39*0.22)
+
+#12
+print((5**2)-(3*7))
+
+#13
+print('"Khal Drogo\'s favorite word is \"athjahakar\""')
+
+#14
+print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+
+#15
+print('Winter ' + 'came ' + 'for ' + 'the ' + 'House ' + 'of ' + 'Frey.')
+
+#16
+print(chr(126))
+print(chr(94))
+print(chr(37))
+
+#17
+motto = 'What Is Dead May Never Die!'
+print(motto)
+
+#18
+name = "Brienna"
+# BEGIN (write your solution here)
+name='anneirB'
+# END
+print(name)
+
+#19
+a = 2
+print(a)
+
+#20
+euros_count = 100
+# BEGIN (write your solution here)
+dollar = euros_count*1.25
+inter_dollar = int(dollar)
+youan = inter_dollar*6.91
+print(dollar)
+print(youan)
+# END
+
+#21
