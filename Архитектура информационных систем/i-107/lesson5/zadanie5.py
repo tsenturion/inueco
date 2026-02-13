29e772f82858e1d29a9a4e1d0a17ea0b08abcc93
ttyyryyhgfdd <128968538+ttyyryyhgfdd@users.noreply.github.com>
2026-02-13 11:35:22 +0500

    zadanie5_code-basics

---
+++ Архитектура информационных систем/i-107/lesson5/zadanie5.py
@@ -0,0 +1,74 @@
+#14
+print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+
+#15
+print('Winter ' + 'came ' + 'for ' + 'the ' + 'House ' + 'of ' + 'Frey.')
+
+#16
+print(chr(126))
+print(chr(94))
+print(chr(37))
+
+#17
+motto = 'What Is Dead May Never Die!'
+print(motto)
+
+#18
+name = "Brienna"
+# BEGIN (write your solution here)
+name='anneirB'
+# END
+print(name)
+
+#19
+a = 2
+print(a)
+
+#20
+euros_count = 100
+# BEGIN (write your solution here)
+dollar = euros_count*1.25
+inter_dollar = int(dollar)
+youan = inter_dollar*6.91
+print(dollar)
+print(youan)
+# END
+
+#21
+info = "We couldn't verify your mother's maiden name."
+intro = "Here is important information about your account security.\n"
+
+first_name = "Joffrey"
+greeting = "Hello"
+
+# BEGIN (write your solution here)
+print(greeting + ', ' + first_name +'!')
+print(intro + info)
+# END
+
+#22
+first_num = 20
+second_num = -100
+print(first_num*second_num)
+
+#23
+king = "Rooms in King Balon's Castles:"
+
+# BEGIN (write your solution here)
+castles = 6
+rooms = 17
+print(king)
+print(castles*rooms)
+# END
+
+#24
+DRAGONS_BORN_COUNT = 3
+
+#25
+stark = "Arya"
+
+# BEGIN (write your solution here)
+print('Do you want to eat,', stark+'?')
+# END
+
+
