15852e76449bd3387c69dcbee3e8209893f124f1
AskaLaw <gureevana18@gmail.com>
2026-02-13 11:07:00 +0500

    Гуреева Анна И-107

---
+++ Архитектура информационных систем/lesson4/Zadanie_1.py
@@ -0,0 +1,98 @@
+# 1 Привет, Мир
+print('Hello, World!')
+
+# 2 Комментарии
+# You know nothing, Jon Snow!
+
+# 3 Инструкции (Statements)
+print("Robert")
+print("Stannis")
+print("Renly")
+
+# 4 Как мы проверяем ваши решения
+print(10)
+
+# 5 Синтаксические ошибки
+print('What Is Dead May Never Die')
+
+# 6 Арифметические операции
+print(81/9)
+
+#7 Операторы
+print(6 - (-81))
+
+#8 Коммутативная операция
+print(3 ** 5)
+print((-8) / (-4))
+
+#9 Композиция операций
+print(8 / 2 + 5 - (-3) /2)
+
+#10 Приоритет
+print(70 * (3 + 4) / (8 + 2))
+
+#11 Числа с плавающей точкой
+print(0.39 * 0.22)
+
+#12 Линтер
+print((5 ** 2) - (3 * 7))
+
+#13 Кавычки
+print('"Khal Drogo\'s favorite word is "athjahakar""')
+
+#14 Экранированные последовательности
+print('- Did Joffrey agree?\n- He did. He also said "I love using \\n".')
+
+#15 Конкатенация
+print('Winter came for the' + ' ' + 'House of' + ' ' + 'Frey' + '.')
+
+#16 Кодировка
+print(chr(126))
+print(chr(94))
+print(chr(37))
+
+#17 Что такое переменная
+motto = 'What Is Dead May Never Die!'
+print(motto)
+
+#18 Изменение переменной
+name = "Brienna"
+name = 'anneirB'
+print(name)
+
+#19 Выбор имени переменной
+bro = 2
+print(bro)
+
+#20 Выражения в определениях
+euros_count = 100
+dollars_coun = 100 * 1.25
+print(dollars_coun)
+yuans_count = dollars_coun * 6.91
+print(yuans_count)
+
+#21 Переменные и конкатенация
+info = "We couldn't verify your mother's maiden name."
+intro = "Here is important information about your account security."
+
+first_name = "Joffrey"
+greeting = "Hello"
+
+print(greeting + ',' + ' ' + first_name + '!')
+print(intro, info, sep='\n')
+
+#22 Именование переменных
+first_nub = 20 
+second_nub = -100
+print(first_nub * second_nub)
+
+#23 Магические числа
+king = "Rooms in King Balon's Castles:"
+big_room = 6
+small_room = 17
+
+print(king)
+print(big_room * small_room)
+
+#24 Константы
+DRAGONS_BORN_COUNT = 3
