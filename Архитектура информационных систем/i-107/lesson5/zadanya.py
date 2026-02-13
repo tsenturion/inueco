f5be508bd82a001654db33db018148fc9f977aae
noyMMeS <honoradla640@gmail.com>
2026-02-13 11:35:15 +0500

    Markov i - 107

---
+++ Архитектура информационных систем/i-107/lesson5/zadanya.py
@@ -0,0 +1,37 @@
+#19
+a = 2
+print(2)
+#20
+euros_count = 100
+dollar_count = 100 * 1.25
+print(dollar_count)
+yuan_count = dollar_count * 6.91
+print(yuan_count)
+#21
+info = "We couldn't verify your mother's maiden name."
+intro = "Here is important information about your account security."
+
+first_name = "Joffrey!"
+greeting = "Hello,"
+
+print( greeting , first_name , )
+print(intro)
+print(info)
+#22
+first_number = 20
+second_number = -100
+print(first_number * second_number)
+#23
+king = "Rooms in King Balon's Castles:"
+Castles = 6
+bomba = 17
+print(king)
+print(Castles * bomba)
+#24
+DRAGONS_BORN_COUNT = 3
+#25
+stark = "Arya"
+
+
+print('Do you want to eat,' , stark+'?')
+# END
