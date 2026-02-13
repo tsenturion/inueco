1b2edebca5334c7f8d3ff66704bb06188fddc050
AndreyLar1 <25301072@live.inueco.ru>
2026-02-13 11:02:11 +0500

    zadanie 4 lesson

---
+++ Архитектура информационных систем/i-107/lesson4/zadanie4.py
@@ -0,0 +1,69 @@
+#Операторы
+
+print(6-(-81))
+
+#Коммутативная операция
+
+print(3**5)
+
+print((-8)/(-4))
+
+#Композиция операций
+
+print(8/2+5-(-3)/2)
+
+#Приоритет
+
+print(70*(3+4)/(8+2))
+
+#Числа с плавающей точкой
+
+print(0.39*0.22)
+
+#Линтер
+
+print( (5 **2)-(3* 7))
+
+#Кавычки
+
+print('"Khal Drogo\'s favorite word is "athjahakar""')
+
+
+
+#Экранированные последовательности
+
+print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+
+#Конкатенация
+
+print('Winter' + ' ' + 'came' + ' ' + 'for' + ' ' + 'the' + ' ' + 'House' + ' ' + 'of' + ' ' + 'Frey.')
+
+#Кодировка
+
+print(chr(126))
+
+print(chr(94))
+
+print(chr(37))
+
+#Что такое переменная
+
+motto = 'What Is Dead May Never Die!'
+
+print(motto)
+
+#Изменение переменной
+
+name = "Brienna"
+
+
+
+# BEGIN (write your solution here)
+
+name = name[::-1]
+
+# END
+
+
+
+print(name)
