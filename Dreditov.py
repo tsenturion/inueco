082ee585bffd713ef7c65e4cddbebacc31ff22b4
totalEXE <totalexe7543@gmail.com>
2026-02-13 11:28:40 +0500

    czxcxzc

--- Dreditov.py
+++ Dreditov.py
@@ -1,35 +1,78 @@
+#1
+
+print('- Did Joffrey agree?\n- He did. He also said "I love using \\n".')
+
+#2
+
+print('Winter ' + 'came ' + 'for '+ 'the ' + 'House ' + 'of '+ 'Frey.')
+
 #3
-a = 3**5
-b = -8
-c = -4
-print(a)
-print(b/c)
+
+print(chr(126))
+print(chr(94))
+print(chr(37))
 
 #4
-a = 8
-b = 2
-c = 5 
-d = -3
-print(a / b + c - d / b)
+
+motto = 'What Is Dead May Never Die!'
+print(motto)
 
 #5
 
-a = 70
-b = 3
-c = 4
-d = 8
-e = 2
-print(a * (b + c) / (d + e))
+name = "anneirB"
+
+# BEGIN (write your solution here)
+
+# END
+
+print(name)
 
 #6
 
-print(0.39 * 0.22)
+brothers_count = 2
+print(brothers_count)
 
 #7
 
-print((5 **2)-(3* 7))
+euros_count = 100
+dollars = euros_count * 1.25
+print(dollars)
+
+yuans = dollars * 6.91
+print(yuans)
 
 #8
 
-print('"Khal Drogo\'s favorite word is "athjahakar""')
+info = "We couldn't verify your mother's maiden name."
+intro = "Here is important information about your account security."
 
+first_name = "Joffrey"
+greeting = "Hello"
+
+print(greeting + ', ' + first_name + '!')
+print(intro + '\n' + info)
+
+#9
+
+first_number = 20
+second_number = -100
+print(first_number * second_number)
+
+#10
+
+king = "Rooms in King Balon's Castles:"
+print(king)
+
+castles_count = 6
+room_per_castle = 17
+total_rooms = castles_count * room_per_castle
+print(total_rooms)
+
+#11
+
+DRAGONS_BORN_COUNT = 3
+
+#12
+
+stark = "Arya"
+print(f'Do you want to eat, {stark}?')
