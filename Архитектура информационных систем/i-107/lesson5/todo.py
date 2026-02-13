c073a62502f33ea811da98234f122e79923d4d88
papazahoditvofis <stalkertuo@gmail.com>
2026-02-13 11:31:49 +0500

    Update todo.py

--- Архитектура информационных систем/i-107/lesson5/todo.py
+++ Архитектура информационных систем/i-107/lesson5/todo.py
@@ -9,3 +9,49 @@ https://stepik.org/catalog/131?free=true
 "Поколение Python": курс для начинающих 
 до 6.3 Не включая
 """
+"""
+print('- Did Joffrey agree?', '- He did. He also said "I love using \\n".',sep='\n')
+
+print('Winter '+'came '+'for '+'the House of Frey.')
+
+print(chr(ord('~')))
+print(chr(ord('^')))
+print(chr(ord('%')))
+
+motto='What Is Dead May Never Die!'
+print(motto)
+
+name = "Brienna"
+name='anneirB'
+print(name)
+
+brothers=2
+print(brothers)
+
+euros_count = 100
+dollar=euros_count*1.25
+print(dollar)
+yuan=dollar*6.91
+print(yuan)
+
+info = "We couldn't verify your mother's maiden name."
+intro = "Here is important information about your account security."
+first_name = "Joffrey"
+greeting = "Hello"
+print(greeting+', '+first_name+'!')
+print(intro,info,sep='\n')
+
+first_numba=20
+second_numba=-100
+print(first_numba*second_numba)
+
+king = "Rooms in King Balon's Castles:"
+king_po_schetu=6
+rooms_in_one_castle=17
+print(king,king_po_schetu*rooms_in_one_castle,sep='\n')
+
+DRAGONS_BORN_COUNT=3
+
+stark = "Arya"
+print(f'Do you want to eat, {stark}?')
+"""
