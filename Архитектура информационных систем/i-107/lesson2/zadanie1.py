2e733ab4ea57be8369f2c8334d8a355a8e115d60
iv33924 <iv33924@gmail.com>
2026-02-20 10:52:19 +0500

    Виноградов и-107

--- Архитектура информационных систем/i-107/lesson2/zadanie1.py
+++ Архитектура информационных систем/i-107/lesson2/zadanie1.py
@@ -54,7 +54,19 @@ print(u)
 # 21 Переменные и конкатенация
 info = "We couldn't verify your mother's maiden name."
 intro = "Here is important information about your account security."
-
 first_name = "Joffrey"
 greeting = "Hello"
-print(greeting + ', ' + first_name +'!' + '\n' + intro + info)
+print(greeting + ', ' + first_name +'!' + '\n' + intro + '\n' + info)
+# 22 Именование переменных
+x_1 = 20
+x_2 = -100
+print(x_1 * x_2)
+# 23 Магические числа
+king = "Rooms in King Balon's Castles:"
+x_6 = 6
+x_17 = 17
+print(king)
+print(x_6 * x_17)
+# 24 Константы
+DRAGONS_BORN_COUNT = 3
+print(12)
