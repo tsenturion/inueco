b006290f651600d54a7b195b4fff8659a2ffe933
3APA3OHbKA <safina_lerochka@bk.ru>
2026-02-27 11:23:32 +0500

    Сафина

---
+++ Архитектура информационных систем/lesson3/безделье.py
@@ -0,0 +1,20 @@
+name = input()
+print('Привет, ' + name)
+free = input()
+print(free + ' - чемпион!')
+Object1 = input()
+print(Object1)
+Level1 = input()
+Level2 = input()
+Level3 = input()
+print(Level3)
+print(Level2)
+print(Level1) #слипик до 2.3
+print("Добрый день, ", name, ".", sep="") #яндекс 2.1
+name1 = "Пользователь"
+print(f"Добрый день, {name1}.")
+print(f"{123:0>9}") # 000000123 (выравнивание по правому краю)
+print(f"{123:0<9}") # 123000000 (выравнивание по левому краю)
+print(f"{123:0^9}") # 000123000 (выравнивание по центру)
+print("Привет, {}!".format(name1))
+
