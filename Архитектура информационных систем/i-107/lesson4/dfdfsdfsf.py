343b88b8e83167b66ebecd4fdf86086578dff305
TheAntongo <antoxagg09@gmail.com>
2026-02-27 11:15:14 +0500

    Ритм жизни

--- Архитектура информационных систем/i-107/lesson4/dfdfsdfsf.py
+++ Архитектура информационных систем/i-107/lesson4/dfdfsdfsf.py
@@ -32,4 +32,48 @@ name = name[::-1]
 # END
 
 print(name)
+#Выбор имени переменной 
+brothers_count = 2
+print(brothers_count)
+#Выражения в определениях
+euros_count = 100  # Исходное количество евро
 
+# BEGIN (write your solution here)
+dollars_count = euros_count * 1.25
+print(dollars_count)
+
+yuans_count = dollars_count * 6.91
+print(yuans_count)
+# END
+#Переменные и конкатенация
+first_name = 'Joffrey'
+greeting = 'Hello'
+info = 'Here is important information about your account security.'
+intro = 'We couldn\'t verify your mother\'s maiden name.'
+
+# BEGIN (write your solution here)
+print(greeting + ', ' + first_name + '!')
+print(info + '\n' + intro)
+# END
+#Именование переменных 
+first_number = 20
+second_number = -100
+print(first_number * second_number)
+#Магические числа
+king = "Rooms in King Balon's Castles:"
+print(king)
+
+# BEGIN (write your solution here)
+rooms_per_castle = 6
+castles_count = 17
+total_rooms = rooms_per_castle * castles_count
+print(total_rooms)
+# END
+# Константы
+DRAGONS_BORN_COUNT = 3
+#Интерполяция
+stark = 'Arya'
+
+# BEGIN (write your solution here)
+print(f'Do you want to eat, {stark}?')
+# END
