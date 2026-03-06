3632c194c63746f10e4c27590b9498af22627c19
3APA3OHbKA <safina_lerochka@bk.ru>
2026-03-06 11:33:17 +0500

    Сафина Валкрия И-107

---
+++ Архитектура информационных систем/lesson2/zadani.py
@@ -0,0 +1,9 @@
+print('Как Вас зовут?')
+name = input()
+print('Здравствуйте, ' + name + '!')
+print('Как дела?')
+otv = input()
+if otv == 'хорошо':
+    print("Я за Вас рада!")
+else:
+    print("Всё наладится!")
