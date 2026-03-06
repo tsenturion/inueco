6f64e83ab01fedcd769374cb3f5215170840e9fe
poofori <poofori.2007@gmail.com>
2026-03-06 11:14:29 +0500

    Звонков Кирилл и-107

---
+++ Архитектура информационных систем/lesson5/zadanie3.py
@@ -0,0 +1,42 @@
+# 15 Конкатенация
+print('Winter ' +  'came ' + 'for ' + 'the ' + 'House ' + 'of ' + 'Frey.')
+# 16 Кодировка
+print(chr(126))
+print(chr(94))
+print(chr(37))
+# 17 Что такое переменная
+motto = 'What Is Dead May Never Die!'
+print(motto)
+# 18 Изменение переменной
+name = "Brienna"
+name = 'anneirB'
+print(name)
+# 19 Выбор имени переменной
+brother = 2
+print(brother)
+# 20 Выражения в определениях
+euros_count = 100
+d = euros_count * 1.25
+print(d)
+u = d * 6.91
+print(u)
+# 21 Переменные и конкатенация
+info = "We couldn't verify your mother's maiden name."
+intro = "Here is important information about your account security."
+first_name = "Joffrey"
+greeting = "Hello"
+print(greeting + ', ' + first_name +'!' + '\n' + intro + '\n' + info)
+# 22 Именование переменных
+x_1 = 20
+x_2 = -100
+print(x_1 * x_2)
+# 23 Магические числа
+king = "Rooms in King Balon's Castles:"
+x_6 = 6
+x_17 = 17
+print(king)
+print(x_6 * x_17)
+# 24 Константы
+DRAGONS_BORN_COUNT = 3
+print(12)
+DRAGONS_BORN_COUNT = 3
