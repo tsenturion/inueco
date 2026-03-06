25277aafc94b232d1391d7dda707f24ac6e77f3b
meowpixel <assing3c@gmail.com>
2026-03-06 11:09:39 +0500

    Тараканов И-107

---
+++ Архитектура информационных систем/i-107/lesson5/test.py
@@ -0,0 +1,110 @@
+#Тараканов Никита группа И-107
+
+# 1 Привет, Мир
+print('Hello, World!')
+
+# 2 Комментарии
+# You know nothing, Jon Snow!
+
+# 3 Инструкции (Statements)
+print('Robert')
+print('Stannis')
+print('Renly')
+
+# 4 Как мы проверяем ваши решения
+print(10)
+
+# 5 Синтаксические ошибки
+print('What Is Dead May Never Die')
+
+# 6 Арифметические операции
+print(81 / 9)
+
+# 7 Оператор
+print (6 + 81)
+
+#8 Коммуникативная операция
+print (3**5)
+print (-8 / -4)
+
+#9 Композиция операций
+print ( 8/2+5--3/2)
+
+#10 Приоритет
+print (70*(3+4)/(8+2))
+
+# 11  Числа с плавающей точкой
+print (0.39*0.22)
+
+#12 Линтер
+print ((5 ** 2) - (3 * 7))
+
+#13 Кавычки
+print ('"Khal Drogo\'s favorite word is "athjahakar""')
+
+#14 Экранированные последовательности
+print ('- Did Joffrey agree?\n- He did. He also said "I love using \\n".')
+
+# 15 Конкатенация
+print ('Winter ' + "came " + "for " + "the " "House " + "of " + "Frey.")
+
+# 16 Кодировка
+print(chr(126))
+print(chr(94))
+print(chr(37))
+
+#17 Что такое переменная
+motto = 'What Is Dead May Never Die!'
+print (motto)
+
+#18 Изменение переменной
+name = "Brienna"
+
+# BEGIN (write your solution here)
+name = "anneirB"
+# END
+
+print(name)
+
+# 19 Выбор имени переменной
+brothers = 2
+print(brothers)
+
+#20 Выражения в определениях
+euros_count = 100
+# BEGIN (write your solution here)
+dollars_count = euros_count * 1.25
+yuans_count = dollars_count * 6.91
+print(dollars_count)
+print(yuans_count)
+# END
+
+#21 Переменные и конкатенация
+info = "We couldn't verify your mother's maiden name."
+intro = "Here is important information about your account security."
+
+first_name = "Joffrey"
+greeting = "Hello"
+
+# BEGIN (write your solution here)
+print(greeting + "," + " " + first_name + "!")
+print(intro + "\n" + info)
+# END
+
+#22 Именование переменных
+first_number = 20
+second_number = -100
+
+cifra = first_number * second_number
+print(cifra)
+
+#23 Магические числа
+king = "Rooms in King Balon's Castles:"
+
+# BEGIN (write your solution here)
+print(king + "\n" + "102")
+# END
+
+#24 Константы
+DRAGONS_BORN_COUNT = 3
+
