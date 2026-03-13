f6821fb9fc4e423ba1af47b80334b393956f269a
noyMMeS <honoradla640@gmail.com>
2026-03-13 11:32:50 +0500

    Марков и 107

---
+++ Архитектура информационных систем/i-107/lesson6/bs.py
@@ -0,0 +1,62 @@
+#26
+stark = "Arya"
+
+# BEGIN (write your solution here)
+print(f'Do you want to eat, {stark}?')
+# END
+
+#27
+name = "Na\nharis"
+
+# BEGIN (write your solution here)
+print(name[7])
+# END
+
+#28
+value = "Hexlet"
+
+# BEGIN (write your solution here)
+print(value[2:5])
+# END
+
+#29
+# BEGIN (write your solution here)
+text = """Lannister, Targaryen, Baratheon, Stark, Tyrell...
+they're all just spokes on a wheel.
+This one's on top, then that one's on top, and on and on it spins,
+crushing those on the ground."""
+# END
+
+print(text)
+
+#30
+print(-0.304)
+
+#31
+a = 7
+print(a - (-8 - (-2)))
+
+#32
+one = "Naharis"
+two = "Mormont"
+three = "Sand"
+
+# BEGIN (write your solution here)
+result = one[2]+two[1]+three[3]+two[4]+two[2]
+print(result)
+#33
+value = 2.9
+
+# BEGIN (write your solution here)
+print(str(int(value)) + ' times')
+# END
+
+#34
+company1 = "Apple"
+company2 = "Samsung"
+
+# BEGIN (write your solution here)
+total_length = len(company1) + len(company2)
+print(total_length)
+# END
+#35
