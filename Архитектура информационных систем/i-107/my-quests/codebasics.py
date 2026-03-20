f1314984f1729ce6c23a0ec606a66925485b5abd
ttyyryyhgfdd-bee <shakirow82@bk.ru>
2026-03-20 11:24:16 +0500

    Шакиров И-107, весь codebasics

--- Архитектура информационных систем/i-107/my-quests/codebasics.py
+++ Архитектура информационных систем/i-107/my-quests/codebasics.py
@@ -172,11 +172,20 @@ text = "When \t\n you play a \t\n game of thrones you win or you die."
 print(len(text[4:15].strip()))
 # END
 
-#41 skipped
+#41
+def print_motto():
+    print("Winter is coming")
 
-#42 skipped
+#42
+def truncate(text, length):
+    # BEGIN (write your solution here)
+    return text[:length]+'...'
+    # END
 
-#43 skipped
+#43
+def get_hidden_card(card_number, stars_count=4):
+    visible_digits = card_number[-4:]
+    return '*' * stars_count + visible_digits
 
 #44
 def trim_and_repeat(text, offset=0, repetitions=1):
@@ -192,3 +201,144 @@ def word_multiply(text: str, n: int) -> str:
 text = 'python'
 print(word_multiply(text, 2))
 print(word_multiply(text, 0))
+
+#46
+def is_pensioner(age):
+    return age >= 60
+print(is_pensioner(59))
+print(is_pensioner(60))
+print(is_pensioner(65))
+
+#47
+def is_long_word(word):
+    return len(word) > 5
+
+#48
+def is_international_phone(phone):
+    return phone[0]=='+'
+
+#49
+def is_leap_year(year):
+    return year%400==0 or (year%4==0 and year%100!=0)
+
+#50
+def is_palindrome(a):
+    b = a.lower()
+    return b == b[::-1]
+def is_not_palindrome(a):
+    return not is_palindrome(a)
+
+#51
+def string_or_not(word):
+    if isinstance(word, str):
+        return 'yes'
+    else:
+        return 'no'
+    
+#52
+def guess_number(num):
+    return 'You win!' if num==42 else 'Try again!'
+
+#53
+def normalize_url(url):
+    if url[:8] == 'https://':
+        return url
+    if url[:7] == 'http://':
+        return 'https://' + url[7:]
+    return 'https://' + url
+
+#54
+def who_is_this_house_to_starks(surname):
+    if surname == 'Karstark' or surname == 'Tully':
+        return 'friend'
+    elif surname == 'Lannister' or surname == 'Frey':
+        return 'enemy'
+    else:
+        return 'neutral' 
+    
+#55
+def flip_flop(word):
+    return 'flop' if word=='flip' else 'flip'
+
+#56
+def get_number_explanation(num):
+    if num == 666:
+        return 'devil number'
+    elif num == 42:
+        return 'answer for everything'
+    elif num == 7:
+        return 'prime number'
+    else:
+        return 'just a number'
+
+
+#57
+def print_reversed_numbers(number):
+    while number > 0:
+        print(number)
+        number -= 1
+    print('finished!')
+
+#58
+def multiply_numbers_from_range(start, end):
+    result = 1
+    for i in range(start, end + 1):
+        result *= i
+    return result
+
+#59
+def join_numbers_from_range(n1,n2):
+    r=''
+    for i in range(n1,n2+1):
+        r+=str(i)
+    return r
+
+#60
+def add_spaces(text):
+    result = ""
+    for char in text:
+        result += char + " "
+    return result.strip()
+
+#61
+def count_chars(text, char):
+    count = 0
+    ltext = text.lower()
+    lchar = char.lower()
+    for cchar in ltext:
+        if cchar == lchar:
+            count += 1
+    return count
+
+#62
+def filter_string(text, char):
+    result = ''
+    i = 0
+    while i < len(text):
+        current_char = text[i]
+        if current_char != char:
+            result += current_char
+        i += 1
+    return result
+
+#63
+def is_contains_char(word, symb):
+    return symb in word
+
+#64
+def filter_string(word, symb):
+    return "".join([char for char in word if char.lower() != symb.lower()])
+
+#65
+def fizzbuzz(n):
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    return " ".join(result)
