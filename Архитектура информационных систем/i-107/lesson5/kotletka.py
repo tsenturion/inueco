6c1992669b5732cc47c9c5e44fd12821fb49838c
Dorir228-afk <dagbl639@gmail.com>
2026-03-20 11:32:00 +0500

    Ячевский Данил И-107

--- Архитектура информационных систем/i-107/lesson5/kotletka.py
+++ Архитектура информационных систем/i-107/lesson5/kotletka.py
@@ -588,5 +588,14 @@ if status == "хорошо":
 elif status == "плохо":
     print("Всё наладится!")
 #END
+#2
+petya_speed = int(input())
+vasya_speed = int(input())
+
+if petya_speed > vasya_speed:
+    print("Петя")
+else:
+    print("Вася")
+#END
 
 
