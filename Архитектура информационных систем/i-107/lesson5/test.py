02b502d630db176ee5e09835a1283a579db2a530
meowpixel <assing3c@gmail.com>
2026-04-10 11:34:26 +0500

    Тараканов И-107

--- Архитектура информационных систем/i-107/lesson5/test.py
+++ Архитектура информационных систем/i-107/lesson5/test.py
@@ -267,5 +267,11 @@ def is_leap_year(year):
         False
     
 #49 Отрицание
+def is_palindrome(word):
+   lower_word = word.lower()
+   return lower_word == lower_word [::-1]
+def is_not_palindrome(word):
+    return not is_palindrome(word)
 
+#50 Результат логических выражений
 
